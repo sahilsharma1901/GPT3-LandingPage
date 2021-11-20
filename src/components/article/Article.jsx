96158c1317a62824aedf73a12c70f48971261aa0
Sahil Sharma <sharma.1901@yahoo.com>
2021-11-20 22:23:39 +0530

    Landing Page completion

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Article.css';
 
-const Article = () => {
+const Article = ({imageURL}) => {
     return (
-        <div>
-            Article
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={imageURL} alt="Blog"/>
+            </div>
+            <div className="gpt3__blog-container_article-content">
+                <div>
+                    <p>Sep 26, 2021</p>
+                    <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     );
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+
+import {Article} from "../../components";
+import {Blog1, Blog2, Blog3, Blog4, Blog5} from "./imports";
+
 import "./Blog.css";
 
 const Blog = () => {
     return (
-        <div>
-            Blog
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">A lot is happening,<br/>we are blogging about it</h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-container_groupA">
+                    <Article imageURL={Blog1}/>
+                </div>
+                <div className="gpt3__blog-container_groupB">
+                    <Article imageURL={Blog2}/>
+                    <Article imageURL={Blog3}/>
+                    <Article imageURL={Blog4}/>
+                    <Article imageURL={Blog5}/>
+                </div>
+            </div>
         </div>
     );
 }
